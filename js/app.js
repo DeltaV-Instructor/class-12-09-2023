dc5eb2772c79f80c612deea3a4a8c39e3134d413
tektechnologies <info@tektechnologies.com>
2023-09-26 10:49:02 -0500

    add js comments for tasks

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
 'use strict';
 console.log('app file is connected');
+
+//global variables for grabbing html elements and we are going to set up some arrays to help us hold some values for the logic we need to render random images to the user..
+
+
+
+//create a constructor function that will create pizza objects that holds data about our pizzas and the clicking info. {pizza info in these objects.}
+
+
+
+
+/**
+ * need to create a function to handle what happens when we click on the pizza
+  add a check to see if we are clicking on a image
+  add to our total clicks to finish rounds
+  update the pizza object information times shown... both images each time - views
+  count what image was clicked  - clicks
+  logic -
+  display our random images we are going random number from our pizza objects array
+  we can then update the "src" in the html to update the image that we are seeing on the page
+  add some logic for images that we just used array to store so we dont select those images again for the next round.
+  lastly I want to count total clicks if statement when reach the end of 5 clicks 
+  remove the event listener so the user can not click any more. 
+
+ *  */
+
+
+//create a function handleResult list showing ul and li on the left side of the page
+
+
+
+
+//Eventually add the chart here form canvas.js big concept not much to code.
+
+
+//add our event listeners for our button clicks  = 'click' events functions
+//call the constructor function to create out pizza objects
+
+
+
+
